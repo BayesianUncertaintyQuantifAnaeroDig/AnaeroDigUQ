eaebd81dc211c7de7b774abf3365217ce46e8ac0
picloiseau <picloiseau@gmail.com>
2023-11-30 14:41:40 +0100

    added doc section

--- aduq/__init__.py
+++ aduq/__init__.py
@@ -3,5 +3,4 @@
 The module contains submodules 'bayes', 'optim', 'proba', 'pyadm1', 'pyam2', 'uncertainty.'
 """
 
-# from . import bayes, misc, optim, proba, pyadm1, pyam2, uncertainty
-from . import proba
+from . import bayes, misc, optim, proba, pyadm1, pyam2, uncertainty
